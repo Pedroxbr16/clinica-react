f2d9cf216d73c8c36536ae1570a591707f6bf271
Eduardo-Alves20 <eduardofernandosalves@gmail.com>
2024-09-14 09:59:32 -0300

    backend

---
+++ clinica_back/src/Entity/Paciente.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PacienteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PacienteRepository::class)]
+class Paciente
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nome = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cpf = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nascimento = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telefone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cep = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cidade = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $bairro = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $rua = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $numero = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getCpf(): ?string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): static
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getNascimento(): ?string
+    {
+        return $this->nascimento;
+    }
+
+    public function setNascimento(string $nascimento): static
+    {
+        $this->nascimento = $nascimento;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelefone(): ?string
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): static
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    public function getCep(): ?string
+    {
+        return $this->cep;
+    }
+
+    public function setCep(string $cep): static
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getCidade(): ?string
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade(string $cidade): static
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    public function getBairro(): ?string
+    {
+        return $this->bairro;
+    }
+
+    public function setBairro(string $bairro): static
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    public function getRua(): ?string
+    {
+        return $this->rua;
+    }
+
+    public function setRua(string $rua): static
+    {
+        $this->rua = $rua;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+}
